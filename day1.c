cdb6d737a92110b298429c252edc984cc573f2fc
ankitamipara <ankitamipara1999@gmail.com>
2024-07-03 10:08:51 +0530

    initial commit

---
+++ day1.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+
+
+int main()
+{
+    printf("\nQ-1\n");
+
+    printf("Ankit Amipara\n");
+    printf("25\n");
+    printf("Red and White\n");
+
+
+    printf("\nQ-2\n");
+    printf("--------\n");
+    printf("|      |\n");
+    printf("R      |\n");
+    printf("N      |\n");
+    printf("W      |\n");
+    printf("|      |\n");
+    printf("--------\n");
+
+    printf("\nQ-3\n");
+    printf("*\n");
+    printf("\n");
+    printf("*\n");
+    printf("\n");
+    printf("*\n");
+
+    printf("\nQ-4\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*      * *       *\n");
+    printf("*     *   *     *\n");
+    printf("*    *     *   *\n");
+    printf("*   *       * *\n");
+    printf("* * \n");
+    printf("*\n");
+
+}
